be6669999f154cdd2050f54f79b1b1b1f07dc1db
Abhilash437 <abhilashhathwar20@gmail.com>
2023-03-23 18:26:03 +0530

    temp commit

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
+from flask import request
 
 app = Flask(__name__)
 
 CORS(app)
 @app.route("/",methods=['POST','GET'])
 def home():
+    if request.method == 'POST':
+      f = request.files['file']
+      return 'file uploaded successfully'
     return "hello"
 
 
