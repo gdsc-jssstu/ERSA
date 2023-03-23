6adb678093451dd5ea2309fe414706f6ff3fcbe2
Srivatsa Joshi <srivatsajoshi180@gmail.com>
2023-03-23 23:54:45 +0530

    unstable patch

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
 from flask import request
+from flask import jsonify
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'backendr/'
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 CORS(app)
 @app.route("/",methods=['POST','GET'])
@@ -14,8 +25,20 @@ def home():
 
 
 @app.route("/soft-story",methods=['POST','GET'])
-def softStory():
-    return "hi"
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No file selected for uploading'}), 400
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify({'message': 'File uploaded successfully'}), 200
+    else:
+        return jsonify({'error': 'Invalid file type'}), 400
 
 if __name__ == "__main__":
    app.run(debug=True)
--- frontend/ERSA-Fronted/src/pages/SoftStory.jsx
+++ frontend/ERSA-Fronted/src/pages/SoftStory.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { useState,useEffect } from 'react';
+
 
 function SoftStory() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileSelect = (event) =>{
+    setSelectedFile(event.target.files[0])
+  }
+
+  const handleSubmit = async (event) =>{
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('picture', selectedFile)
+    const response = await fetch('http://127.0.0.1:5000/soft-story', {
+      method: 'POST',
+      body: formData
+    })
+  }
+
   return (
     <div>
         <h1>
             Soft Story Page of ERSA Frontend 🤠
         </h1>
         <Link to="/">Home</Link>
+        <form onSubmit={handleSubmit}>
+            <label>
+              Select a picture:
+              <input type="file" onChange={handleFileSelect} />
+            </label>
+            <button type="submit">Upload</button>
+        </form>
     </div>
   )
 }
