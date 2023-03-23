e178306ae1b5919fc643ac013b92ae37663ecc6c
Abhilash437 <abhilashhathwar20@gmail.com>
2023-03-23 17:51:06 +0530

    Add paths

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 CORS(app)
-@app.route("/",methods=['POST'])
+@app.route("/",methods=['POST','GET'])
 def home():
     return "hello"
+
+
+@app.route("/soft-story",methods=['POST','GET'])
+def softStory():
+    return "hi"
