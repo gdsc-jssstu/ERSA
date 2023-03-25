e5cac45a622b376aae23305d2f4d60319233f6f1
Abhilash437 <abhilashhathwar20@gmail.com>
2023-03-25 22:42:44 +0530

    temp commit

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,23 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_cors import CORS,cross_origin
 from flask import request
 from flask import jsonify
 from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
+CORS(app)
 
-UPLOAD_FOLDER = 'backendr/'
+UPLOAD_FOLDER = '/backend/images'
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-CORS(app)
 @app.route("/",methods=['POST','GET'])
 def home():
     if request.method == 'POST':
@@ -24,21 +26,34 @@ def home():
     return "hello solution challenge"
 
 
-@app.route("/soft-story",methods=['POST','GET'])
+@app.route("/soft-story",methods=['POST'])
 def upload_file():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part in the request'}), 400
+    # if 'file' not in request.files:
+    #     return jsonify({'error': 'No file part in the request'}), 400
 
-    file = request.files['file']
-    if file.filename == '':
+    # file = request.files['picture']
+    # if file.filename == '':
+    #     return jsonify({'error': 'No file selected for uploading'}), 400
+
+    # if file and allowed_file(file.filename):
+    #     filename = secure_filename(file.filename)
+    #     #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #     return jsonify({'message': 'File uploaded successfully'}), 200
+    # else:
+    #     return jsonify({'error': 'Invalid file type'}), 400
+    if 'picture' not in request.files:
+        return 'No image uploaded', 400
+
+    image = request.files['picture']
+
+    if image.filename == '':
         return jsonify({'error': 'No file selected for uploading'}), 400
+    
+    #image.save('/path/to/save/image')
+    if image and allowed_file(image.filename):
+      #image.save('backend/images')
+      return 'Image uploaded successfully', 200
 
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return jsonify({'message': 'File uploaded successfully'}), 200
-    else:
-        return jsonify({'error': 'Invalid file type'}), 400
 
 if __name__ == "__main__":
    app.run(debug=True)
--- frontend/ERSA-Fronted/src/pages/SoftStory.jsx
+++ frontend/ERSA-Fronted/src/pages/SoftStory.jsx
@@ -1,24 +1,47 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import { useState,useEffect } from 'react';
+import axios from 'axios';
 
 
 function SoftStory() {
   const [selectedFile, setSelectedFile] = useState(null);
 
   const handleFileSelect = (event) =>{
-    setSelectedFile(event.target.files[0])
+    setSelectedFile(event.target.files[0]);
+    //console.log(selectedFile);
   }
 
-  const handleSubmit = async (event) =>{
+  const handleSubmit = (event) =>{
     event.preventDefault();
     const formData = new FormData();
+    //console.log(selectedFile);
     formData.append('picture', selectedFile)
-    const response = await fetch('http://127.0.0.1:5000/soft-story', {
-      method: 'POST',
-      body: formData
-    })
+    // const response = await fetch('http://127.0.0.1:5000/soft-story', {
+    //   method: 'POST',
+    //   mode:'cors',
+    //   headers: {
+    //     'Content-type': 'application/json',
+    //   },
+    //   body: formData
+    // }).then((res)=>console.log(res)).catch((err)=>console.log(err))
+    axios.post("http://127.0.0.1:5000/soft-story",
+    formData,
+    {
+      headers: {
+      'Content-Type': 'multipart/form-data',
+    },}
+    )
+    .then((res)=>console.log(res))
+    .catch((err)=>console.log(err))
   }
+
+  // useEffect(()=>{
+  //   const res = async () => {
+  //     await axios.get("http://127.0.0.1:5000/dummy").then((res)=>console.log(res)).catch((err)=>console.log(err))
+  //   }
+  //   res();
+  // },[])
 
   return (
     <div>
